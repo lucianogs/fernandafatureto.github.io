db74c2eba11cabd06ddd1dcf8d6fcd60c5fc3545
Luciano G. Souza <luciano.zip@gmail.com>
2015-05-26 20:24:41 -0300

    Commit-final1
    Commit-final1

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# FernandaFatureto
+website/blog
---
+++ envia.php
@@ -0,0 +1,28 @@
+<?php
+
+// Passando os dados obtidos pelo formulário para as variáveis abaixo
+$nomeremetente     = $_POST['name'];
+$emailremetente    = trim($_POST['email']);
+$emaildestinatario = 'contato@fernandafatureto.com.br'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
+$mensagem          = $_POST['message'];
+ 
+ 
+/* Montando a mensagem a ser enviada no corpo do e-mail. */
+$mensagemHTML = '<P>FORMULARIO PREENCHIDO NO SITE WWW.FERNANDAFATURETO.COM.BR</P>
+<p><b>Nome:</b> '.$name.'
+<p><b>E-Mail:</b> '.$email.'
+<p><b>Mensagem:</b> '.$message.'</p>
+<hr>';
+
+
+// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
+// O return-path deve ser ser o mesmo e-mail do remetente.
+$headers = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+$headers .= "From: $emailremetente\r\n"; // remetente
+$headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
+$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers); 
+
+//if($envio)
+//echo "<div class="modal small"><button type="button" class="btn btn-primary">E-mail enviado com sucesso!</button></div>"; // Página que será redirecionada
+?>
